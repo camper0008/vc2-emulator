da0c572e68a1af0cb9ac269b85d240f46e5bf4ee
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-10-09 11:48:50 +0200

    parse jmp

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -35,11 +35,12 @@ impl TryFrom<u8> for Register {
             0b01 => Ok(Self::GeneralPurpose1),
             0b10 => Ok(Self::Flag),
             0b11 => Ok(Self::ProgramCounter),
-            value => Err(format!("invalid register 0b{value:2b}")),
+            value => Err(format!("invalid register 0b{value:b}")),
         }
     }
 }
 
+#[derive(Debug)]
 #[repr(u8)]
 pub enum Selector {
     Register = 0b00,
@@ -57,12 +58,12 @@ impl TryFrom<u8> for Selector {
             0b01 => Ok(Self::Immediate),
             0b10 => Ok(Self::RegisterAddress),
             0b11 => Ok(Self::ImmediateAddress),
-            value => Err(format!("invalid selector 0b{value:2b}")),
+            value => Err(format!("invalid selector 0b{value:b}")),
         }
     }
 }
 
-pub enum MOV {
+pub enum Target {
     RegisterFromRegister(Register, Register),
     RegisterFromImmediate(Register, Immediate),
     RegisterFromRegisterAddress(Register, Register),
@@ -73,30 +74,46 @@ pub enum MOV {
     ImmediateAddressFromImmediate(Immediate, Immediate),
 }
 
-pub enum MathOp {
+pub enum RegisterDestinationTarget {
     Register(Register, Register),
     Immediate(Register, Immediate),
     RegisterAddress(Register, Register),
     ImmediateAddress(Register, Immediate),
 }
 
+pub enum JmpTarget {
+    Register(Register),
+    Immediate(Immediate),
+    RegisterAddress(Register),
+    ImmediateAddress(Immediate),
+}
+
 pub enum Instruction {
     Htl,
-    Mov(MOV),
+    Mov(Target),
     Not(Register),
-    Or(MathOp),
-    And(MathOp),
-    Xor(MathOp),
-    Shl(MathOp),
-    Shr(MathOp),
-    Add(MathOp),
-    Sub(MathOp),
-    Mul(MathOp),
-    IMul(MathOp),
-    Div(MathOp),
-    IDiv(MathOp),
-    Rem(MathOp),
-    Cmp(MathOp),
+    Or(RegisterDestinationTarget),
+    And(RegisterDestinationTarget),
+    Xor(RegisterDestinationTarget),
+    Shl(RegisterDestinationTarget),
+    Shr(RegisterDestinationTarget),
+    Add(RegisterDestinationTarget),
+    Sub(RegisterDestinationTarget),
+    Mul(RegisterDestinationTarget),
+    IMul(RegisterDestinationTarget),
+    Div(RegisterDestinationTarget),
+    IDiv(RegisterDestinationTarget),
+    Rem(RegisterDestinationTarget),
+    Cmp(RegisterDestinationTarget),
+    Jmp(JmpTarget),
+    Jz(Target),
+    Jnz(Target),
+    Jeq(Target),
+    Jne(Target),
+    Jge(Target),
+    Jgt(Target),
+    Jle(Target),
+    Jlt(Target),
 }
 
 type ParseResult = Result<Vec<Instruction>, String>;
@@ -107,6 +124,92 @@ impl<Iter: Iterator<Item = u8>> Parser<Iter> {
     }
     pub fn parse(self) -> ParseResult {
         self.parse_internal(Vec::new(), 0)
+    }
+    pub fn parse_target(
+        &mut self,
+        instruction_name: &str,
+        op: usize,
+        destination_selector: Selector,
+        source_selector: Selector,
+        destination: Result<Register, String>,
+        source: Result<Register, String>,
+    ) -> Result<(Target, usize), String> {
+        macro_rules! consume_immediate {
+            ($error: expr) => {
+                u32::from_be_bytes([
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                ])
+            };
+        }
+
+        let (instruction, immediates_consumed) = match (destination_selector, source_selector) {
+            (Selector::Register, Selector::Register) => {
+                (Target::RegisterFromRegister(destination?, source?), 0)
+            }
+            (Selector::Register, Selector::Immediate) => (
+                Target::RegisterFromImmediate(
+                    destination?,
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} reg imm' instruction at {op}"
+                    ),
+                ),
+                1,
+            ),
+            (Selector::Register, Selector::RegisterAddress) => (
+                Target::RegisterFromRegisterAddress(destination?, source?),
+                0,
+            ),
+            (Selector::Register, Selector::ImmediateAddress) => (
+                Target::RegisterFromImmediateAddress(
+                    destination?,
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} reg [imm]' instruction at {op}"
+                    ),
+                ),
+                1,
+            ),
+            (Selector::RegisterAddress, Selector::Register) => (
+                Target::RegisterAddressFromRegister(destination?, source?),
+                0,
+            ),
+            (Selector::RegisterAddress, Selector::Immediate) => (
+                Target::RegisterAddressFromImmediate(
+                    destination?,
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} [reg] imm' instruction at {op}"
+                    ),
+                ),
+                1,
+            ),
+            (Selector::ImmediateAddress, Selector::Register) => (
+                Target::ImmediateAddressFromRegister(
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} [imm] reg' instruction at {op}"
+                    ),
+                    source?,
+                ),
+                1,
+            ),
+            (Selector::ImmediateAddress, Selector::Immediate) => (
+                Target::ImmediateAddressFromImmediate(
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} [imm] imm' instruction at {op}"
+                    ),
+                    consume_immediate!(
+                        "no immediate after '{instruction_name} [imm] imm' instruction at {op}"
+                    ),
+                ),
+                2,
+            ),
+            variant => Err(format!(
+                "invalid selector/destination combo '{variant:?}' at {op}"
+            ))?,
+        };
+
+        Ok((instruction, immediates_consumed))
     }
 
     fn parse_mov(
@@ -119,75 +222,22 @@ impl<Iter: Iterator<Item = u8>> Parser<Iter> {
             .next()
             .ok_or(format!("no value after 'MOV' instruction at {op}"))?;
 
-        let variant = (input & 0b1111_0000) >> 4;
-        let destination = (input & 0b0000_1100) >> 2;
-        let destination: Register = destination.try_into()?;
-        let source = input & 0b0000_0011;
-        let source: Register = source.try_into()?;
+        let destination_selector: Selector = ((input & 0b1100_0000) >> 6).try_into()?;
+        let source_selector: Selector = ((input & 0b0011_0000) >> 4).try_into()?;
 
-        macro_rules! consume_immediate {
-            ($error: expr) => {
-                u32::from_be_bytes([
-                    self.feed.next().ok_or(format!($error))?,
-                    self.feed.next().ok_or(format!($error))?,
-                    self.feed.next().ok_or(format!($error))?,
-                    self.feed.next().ok_or(format!($error))?,
-                ])
-            };
-        }
+        let destination: Result<Register, _> = ((input & 0b0000_1100) >> 2).try_into();
+        let source: Result<Register, _> = (input & 0b0000_0011).try_into();
 
-        let (instruction, immediates_consumed) = match variant {
-            0b0000 => (
-                Instruction::Mov(MOV::RegisterFromRegister(destination, source)),
-                0,
-            ),
-            0b0001 => (
-                Instruction::Mov(MOV::RegisterFromImmediate(
-                    destination,
-                    consume_immediate!("no immediate after 'MOV reg imm' instruction at {op}"),
-                )),
-                1,
-            ),
-            0b0010 => (
-                Instruction::Mov(MOV::RegisterFromRegisterAddress(destination, source)),
-                0,
-            ),
-            0b0011 => (
-                Instruction::Mov(MOV::RegisterFromImmediateAddress(
-                    destination,
-                    consume_immediate!("no immediate after 'MOV reg [imm]' instruction at {op}"),
-                )),
-                1,
-            ),
-            0b1000 => (
-                Instruction::Mov(MOV::RegisterAddressFromRegister(destination, source)),
-                0,
-            ),
-            0b1001 => (
-                Instruction::Mov(MOV::RegisterAddressFromImmediate(
-                    destination,
-                    consume_immediate!("no immediate after 'MOV [reg] imm' instruction at {op}"),
-                )),
-                1,
-            ),
-            0b1100 => (
-                Instruction::Mov(MOV::ImmediateAddressFromRegister(
-                    consume_immediate!("no immediate after 'MOV [imm] reg' instruction at {op}"),
-                    source,
-                )),
-                1,
-            ),
-            0b1101 => (
-                Instruction::Mov(MOV::ImmediateAddressFromImmediate(
-                    consume_immediate!("no immediate after 'MOV [imm] imm' instruction at {op}"),
-                    consume_immediate!("no immediate after 'MOV [imm] imm' instruction at {op}"),
-                )),
-                2,
-            ),
-            variant => Err(format!("invalid mov variant '0b{variant:4b}' at {op}"))?,
-        };
+        let (instruction, immediates_consumed) = self.parse_target(
+            "MOV",
+            op,
+            destination_selector,
+            source_selector,
+            destination,
+            source,
+        )?;
 
-        instructions.push(instruction);
+        instructions.push(Instruction::Mov(instruction));
 
         self.parse_internal(instructions, op + 1 + immediates_consumed * 4)
     }
@@ -206,6 +256,90 @@ impl<Iter: Iterator<Item = u8>> Parser<Iter> {
         instructions.push(Instruction::Not(destination));
         self.parse_internal(instructions, op + 1)
     }
+    fn parse_jmp(mut self, mut instructions: Vec<Instruction>, op: usize) -> ParseResult {
+        let input = self
+            .feed
+            .next()
+            .ok_or(format!("no value after 'MOV' instruction at {op}"))?;
+
+        let selector = ((input & 0b1100_0000) >> 6).try_into()?;
+        let destination = ((input & 0b0000_1100) >> 2).try_into();
+
+        macro_rules! consume_immediate {
+            ($error: expr) => {
+                u32::from_be_bytes([
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                    self.feed.next().ok_or(format!($error))?,
+                ])
+            };
+        }
+
+        let (instruction, immediates_consumed) = match selector {
+            Selector::Register => (JmpTarget::Register(destination?), 0),
+            Selector::Immediate => (
+                JmpTarget::Immediate(consume_immediate!(
+                    "no immediate after 'JMP' instruction at {op}"
+                )),
+                1,
+            ),
+            Selector::RegisterAddress => (JmpTarget::RegisterAddress(destination?), 0),
+            Selector::ImmediateAddress => (
+                JmpTarget::ImmediateAddress(consume_immediate!(
+                    "no immediate after 'JMP' instruction at {op}"
+                )),
+                1,
+            ),
+        };
+
+        instructions.push(Instruction::Jmp(instruction));
+
+        self.parse_internal(instructions, op + 1 + immediates_consumed * 4)
+    }
+
+    fn parse_conditional_jmp(
+        mut self,
+        field: NamedInstruction,
+        mut instructions: Vec<Instruction>,
+        op: usize,
+    ) -> ParseResult {
+        let name = format!("{field:?}");
+        let constructor = match field {
+            named_instruction::Jz => Instruction::Jz,
+            named_instruction::Jnz => Instruction::Jnz,
+            named_instruction::Jeq => Instruction::Jeq,
+            named_instruction::Jne => Instruction::Jne,
+            named_instruction::Jge => Instruction::Jge,
+            named_instruction::Jgt => Instruction::Jgt,
+            named_instruction::Jle => Instruction::Jle,
+            named_instruction::Jlt => Instruction::Jlt,
+            field => Err(format!("invalid instruction {field:?}"))?,
+        };
+
+        let input = self
+            .feed
+            .next()
+            .ok_or(format!("no value after '{name}' instruction at {op}"))?;
+
+        let destination_selector: Selector = ((input & 0b1100_0000) >> 6).try_into()?;
+        let source_selector: Selector = ((input & 0b0011_0000) >> 4).try_into()?;
+        let destination: Result<Register, _> = ((input & 0b0000_1100) >> 2).try_into();
+        let source: Result<Register, _> = (input & 0b0000_0011).try_into();
+
+        let (field, immediates_consumed) = self.parse_target(
+            &name,
+            op,
+            destination_selector,
+            source_selector,
+            destination,
+            source,
+        )?;
+
+        instructions.push(constructor(field));
+        self.parse_internal(instructions, op + 1 + immediates_consumed)
+    }
+
     fn parse_math_op(
         mut self,
         field: NamedInstruction,
@@ -254,17 +388,20 @@ impl<Iter: Iterator<Item = u8>> Parser<Iter> {
         }
 
         let (field, immediates_consumed) = match selector {
-            Selector::Register => (MathOp::Register(destination, source), 0),
+            Selector::Register => (RegisterDestinationTarget::Register(destination, source), 0),
             Selector::Immediate => (
-                MathOp::Immediate(
+                RegisterDestinationTarget::Immediate(
                     destination,
                     consume_immediate!("no immediate after '{name} reg imm' instruction at {op}"),
                 ),
                 1,
             ),
-            Selector::RegisterAddress => (MathOp::RegisterAddress(destination, source), 0),
+            Selector::RegisterAddress => (
+                RegisterDestinationTarget::RegisterAddress(destination, source),
+                0,
+            ),
             Selector::ImmediateAddress => (
-                MathOp::ImmediateAddress(
+                RegisterDestinationTarget::ImmediateAddress(
                     destination,
                     consume_immediate!("no immediate after '{name} reg imm' instruction at {op}"),
                 ),
@@ -303,7 +440,17 @@ impl<Iter: Iterator<Item = u8>> Parser<Iter> {
                 | named_instruction::Cmp),
             ) => self.parse_math_op(v, instructions, op + 1),
             Some(named_instruction::Not) => self.parse_not(instructions, op + 1),
-            Some(_) => todo!(),
+            Some(named_instruction::Jmp) => self.parse_jmp(instructions, op + 1),
+            Some(
+                v @ (named_instruction::Jz
+                | named_instruction::Jnz
+                | named_instruction::Jeq
+                | named_instruction::Jne
+                | named_instruction::Jge
+                | named_instruction::Jgt
+                | named_instruction::Jle
+                | named_instruction::Jlt),
+            ) => self.parse_conditional_jmp(v, instructions, op + 1),
             None => Ok(instructions),
         }
     }
