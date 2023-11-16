41d99c38238f886bbfaf17dc1d075f8d40c08de8
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-11-16 12:32:02 +0100

    programs: infinite_add -> add

--- programs/add.asm
+++ programs/add.asm
@@ -1,1 +1,6 @@
-add r0, 1000
+add r0, 25
+main:
+    add r1, 1
+    sub r0, 1
+    jnz main, r0
+jmpabs 0x3FFF
--- programs/infinite_add.asm
+++
@@ -1,6 +0,0 @@
-add r0, 25
-main:
-    add r1, 1
-    sub r0, 1
-    jnz main, r0
-jmp 0x500
