83815d327072df5b2d17c2f2ecccefe6d0d1b99b
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-11-23 15:07:43 +0100

    programs: blackjack draw random cards

--- programs/blackjack/blackjack.asm
+++ programs/blackjack/blackjack.asm
@@ -1,3 +1,5 @@
+jmp main
+
 ; [0x1000-0x100F] = temp variable
 ; eq
 %define cmp_equal 0x04
@@ -25,9 +27,12 @@ dealer_cards_len: dw 0
 dealer_cards_bp: dw 0
 %offset_word 1
 
-; one bit per card, starting from the left with light red 0-12, dark red 0-12, light blue 0-12, dark blue 0-12
-taken_cards_0: dw 0b11111111_11111111_11111111_11111111
-taken_cards_1: dw 0b11111111_11111111_11110000_00000000
+; one bit per card, starting from the right
+taken_card_index: dw 0
+; light red 0-12, dark red 0-12, light blue 0-12, dark blue 0-12
+taken_cards: 
+    dw 0b11111111111111111111111111111111
+    dw 0b00000000000111111111111111111111
 
 ; background colors
 %define clr_general_bg 0x44444400
@@ -36,73 +41,149 @@ taken_cards_1: dw 0b11111111_11111111_11110000_00000000
 %define clr_light_blue 0xABA4CB00
 %define clr_dark_blue 0x24203800
 
-; screen
+; io
 %define vram_location 0x2034
 %define screen_width 0x2038
 %define screen_height 0x203C
+%define keyboard_callback 0x202C
+%define keyboard_keycode 0x2028
 
 ; image + background
 ; r0 = start offset
 image_draw_return_address: dw 0 ; as done by linker in the case of image
 
-main:
-    .bg:
-        mov r1, [screen_width]
-        mov [rect_width], r1
-        mov r1, [screen_height]
-        mov [rect_height], r1
-        mov [rect_color], clr_general_bg
-        mov r0, 0
-        mov [image_draw_return_address], bg_done
-        jmp draw_rect
-    bg_done:
-        mov [image_draw_return_address], cards_done
-        
-        ; user cards
-        mov [user_cards_len], 7
-        mov [user_cards_bp], 0b01000000_01010001_01100010_01110011
-        mov r1, user_cards_bp
-        add r1, 4
-        mov [r1], 0b01000100_01010101_01100110_1111_1111
-        
-        ; dealer cards
-        mov [dealer_cards_len], 7
-        mov [dealer_cards_bp], 0b01_11_0111__01_00_1000__01_01_1001__01_10_1010
-        mov r1, dealer_cards_bp
-        add r1, 4
-        mov [r1], 0b01111011_01001100_00000001_1111_1111
+%define seed_modulus 0x8000_0000
+%define seed_multiplier 1103515245
+%define seed_increment 12345
+seed: dw 180504
+seed_increment_return_address: dw 0
 
+increment_seed:
+    mul [seed], seed_multiplier
+    add [seed], seed_increment
+    rem [seed], seed_modulus
+    jmp [seed_increment_return_address]
+
+main:
+    mov [keyboard_callback], .randomness_is_seeded
+    mov [seed_increment_return_address], increment_seed
+    jmp increment_seed
+    .randomness_is_seeded:
+    mov [keyboard_callback], 0
+
+    ; .bg:
+    ;     mov r1, [screen_width]
+    ;     mov [rect_width], r1
+    ;     mov r1, [screen_height]
+    ;     mov [rect_height], r1
+    ;     mov [rect_color], clr_general_bg
+    ;     mov r0, 0
+    ;     mov [image_draw_return_address], .bg_done
+    ;     jmp draw_rect
+
+    .bg_done:
+        mov [push_card_len_addr], user_cards_len
+        mov [push_card_return_address], .l0
+        jmp push_card
+        .l0:
+        mov [push_card_return_address], .l1
+        jmp push_card
+        .l1:
+        mov [push_card_return_address], .l2
+        jmp push_card
+        .l2:
+        mov [push_card_len_addr], dealer_cards_len
+        mov [push_card_return_address], .l3
+        jmp push_card
+        .l3:
+        mov [push_card_len_addr], dealer_cards_len
+        mov [push_card_return_address], .l4
+        jmp push_card
+        .l4:
+
+        mov [image_draw_return_address], .cards_done
         jmp draw_cards
 
-cards_done:
-    jmp 0xFFFFFFFF
+    .cards_done:
+        mov r0, [seed]
+        jmp 0xFFFFFFFF
 
+set_card_from_index:
+    ; card_mask = [taken_cards + r1] >> r0
+    ; check if card == 1
+    mov r0, [taken_card_index]
+    mov r1, r0
+    rem r0, 32
+    sub r1, r0
+    div r1, 8 ; (x / 32) * 4
 
-; push_card_return_address: dw 0
-; push_card_value: dw 0
-; push_card_len_addr: dw 0
-; push_card:
-;     ; bp = len_ptr + 4
-
-;     ; r0 = (*cards_len - inner_idx) / 4
-;     mov r0, [push_card_len_addr]
-;     mov r0, [r0]
-;     mov r1, r0
-;     rem r1, 4
-;     sub r0, r1
-;     div r0, 4
-
-;     ; r0 = outer_idx + cards_bp
-;     add r0, 4
-;     add r0, [push_card_len_addr]
+    ; ran out of registers
+    mov fl, 1
+    shl fl, r0
     
-;     ; r1 = word >> inner_idx * 8
-;     mul r1, 8
-;     shr [push_card_value], r1
-;     mov r1, [push_card_value]
-;     ; [r0] |= word
-;     or [r0], r1
-;     jmp [push_card_return_address]
+    add r1, taken_cards
+    and fl, [r1]
+    
+    jz push_card@select_card, fl
+    ; flip bit bc taken
+    not fl
+    and [r1], fl
+
+    ; set value based on card
+    ; revealed
+    mov [push_card_value], 0b0100_0000
+    ; color
+    mov r0, [taken_card_index]
+    div r0, 4
+    shl r0, 4
+    or [push_card_value], r0
+    ; type
+    mov r0, [taken_card_index]
+    rem r0, 13
+    or [push_card_value], r0
+    ; adjust
+    shl [push_card_value], 24
+    jmp push_card@card_set
+
+push_card_return_address: dw 0
+push_card_value: dw 0
+push_card_len_addr: dw 0
+push_card:
+    .select_card:
+        mov [seed_increment_return_address], .seed_incremented
+        .seed_incremented:
+        mov r0, [seed]
+        add [taken_card_index], r0
+        rem [taken_card_index], 53
+        jmp set_card_from_index
+
+    .card_set:
+
+    ; bp = len_ptr + 4
+    ; r0 = (*cards_len - inner_idx) / 4
+    ; reset any carrying bit from seed incrementing
+    and fl, 0
+
+    mov r0, [push_card_len_addr]
+    mov r0, [r0]
+    mov r1, r0
+    rem r1, 4
+    sub r0, r1
+    div r0, 4
+
+    ; r0 = outer_idx + cards_bp
+    add r0, 4
+    add r0, [push_card_len_addr]
+    
+    ; r1 = word >> inner_idx * 8
+    mul r1, 8
+    shr [push_card_value], r1
+    mov r1, [push_card_value]
+    ; [r0] |= word
+    or [r0], r1
+    mov r0, [push_card_len_addr]
+    add [r0], 1
+    jmp [push_card_return_address]
 
 draw_cards:
     %define .saved_return_address 0x1210
@@ -355,5 +436,4 @@ draw_rect:
 
     jmp [image_draw_return_address]
 
-done:
-    jmp 0xFFFFFF
+    
